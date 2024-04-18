30d9c6bedf45f2695523755a2eed20fb5c7b668c
Fidel Esteban Morales Cifuentes <fidel.morales@gatech.edu>
2024-04-18 08:56:24 -0600

    cleanup

--- subtitle processor.py
+++ subtitle processor.py
@@ -7,7 +7,7 @@ def extract_dialog_from_srt(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         content = file.readlines()
 
-    # A state to keep track of whether the next lines should be treated as dialog
+    # Keep track of whether the next lines should be treated as dialog
     is_dialogue = False
     for line in content:
         if is_dialogue:
@@ -23,22 +23,21 @@ def extract_dialog_from_srt(file_path):
 
 def process_subtitle_directories(directory, output_file):
     with open(output_file, 'w', encoding='utf-8') as output:
-        # Traverse the directory and its subdirectories
+
         for root, dirs, files in os.walk(directory):
-            if root.endswith("_subtitles"):  # Ensures we're in a subtitle directory
+            if root.endswith("_subtitles"):
                 module_name = root.replace("_subtitles", "").replace(directory, "").strip("/")
                 output.write(f"\n\n# Module: {module_name.replace('_', ' ')}\n")
+
                 for file in files:
                     if file.endswith(".srt"):
-                        # Construct the full file path
                         file_path = os.path.join(root, file)
                         dialogues = extract_dialog_from_srt(file_path)
                         output.write("\n".join(dialogues) + "\n")
 
 
-# Path to the directory containing all the modules and subtitle directories
 base_directory = '/Users/fides/Documents/MSCS/CS7641-ML/Lectures'
-output_file_path = '/Users/fides/Documents/MSCS/CS7641-ML/Lectures/merged.txt'
+output_file_path = f'{base_directory}/lectures_transcript.txt'
 
 # Call the function with the path to the base directory where subtitle directories are located
 process_subtitle_directories(base_directory, output_file_path)
